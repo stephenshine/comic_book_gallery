deff79b995e430b7340cb881caa2adad32cc66ac
Stephen Shine <stephen.shine88@gmail.com>
2016-07-19 20:21:36 +0100

    Added Detail method
    added the first detail method and added an if statement to redirect too.

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,13 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic book controler!");
+        }
     }
 }
